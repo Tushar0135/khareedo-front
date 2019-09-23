08ebd29ffaaff0d857b45bacced238f5612e6ef7
Tushar <tushartuteja.india@gmail.com>
2019-09-23 14:14:55 +0530

    Second Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { UserCartComponent } from './user-cart/user-cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    NavBarComponent,
+    ProductDetailsComponent,
+    ProductListComponent,
+    SignUpComponent,
+    UserCartComponent
   ],
   imports: [
     BrowserModule,
